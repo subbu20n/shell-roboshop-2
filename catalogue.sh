186b35217e0ed9cd2750352914034118a56859c4
subbu <subbunandyala48@gmail.com>
2025-05-22 20:02:43 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -10,7 +10,7 @@ nodejs_setup
 systemd_setup
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf indtallm mongodb-mongosh -y
+dnf indtallm mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb client"
 
 STATUS=$(mongosh --host mongodb.subbuaws.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
@@ -19,7 +19,7 @@ then
     mongosh --host mongodb.subbuaws.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into mongodb"
 else
-    echo -e "data is already loaded ... $Y SKIPPING $N"
+    echo -e "data is already loaded ... $Y SKIPPING $N" &>>$LOG_FILE
 fi        
 
 print_time | tee -a $LOG_FILE
--- common.sh
+++ common.sh
@@ -10,9 +10,59 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs/"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started execution at: $(date)" | tee -a $LOG_FILE
+
+app_setup(){
+    id roboshop 
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user roboshop"
+else 
+    echo -e "system user roboshop already created ... $Y SKIPPING $N"
+fi
+
+    mkdir -p /app &>>$LOG_FILE
+    VALIDATE $? "creating app directory"
+
+     curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOG_FILE
+     VALIDATE $? "Downloading $app_name"
+
+     rm -rf /app/*
+     cd /app
+     unzip /tmp/$app_name.zip &>>$LOG_FILE
+     VALIDATE $? "unzipping $app_name"
+
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? " Disabling nodejs -y"
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enabling nodejs:20 -y"
+
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Installing nodejs:20"
+
+    cd /app
+    npm install &>>$LOG_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Copying $app_name service"
+
+    systemctl daemon-reload &>>$LOG_FILE
+    systemctl enable $app_name &>>$LOG_FILE
+    systemctl start $app_name
+    VALIDATE $? "Starting $app_name"
+}
+
 
 check_root(){
 if [ $USERID -ne 0 ]
