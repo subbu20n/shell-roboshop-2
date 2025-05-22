bbac9b16cb01b1151b678b0cd8ab67e36d117070
subbu <subbunandyala48@gmail.com>
2025-05-22 20:42:02 +0530

    hello world started

--- redis.sh
+++ redis.sh
@@ -5,22 +5,22 @@ app_name=redis
 
 check_root
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enablig redis"
 
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "INstalling redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Edited redis conf file to accept remote connections"
 
-systemctl enable redis 
+systemctl enable redis  &>>$LOG_FILE
 VALIDATE $? "Enabling redis"
 
-systemctl start redis 
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting redis"
 
 print_time | tee -a $LOG_FILE
