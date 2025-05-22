db6bbdcf0bab97dc13c990e3f2bdc627659c0333
subbu <subbunandyala48@gmail.com>
2025-05-22 22:04:15 +0530

    hello world started

---
+++ rabbitmq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=rabbitmq
+
+check_root
+
+echo "please enter rabbitmq password to setup"
+read -s $RABBITMQ_PASSWD
+
+dnf install rabbitmq-server -y &>>$LOG_FILE 
+VALIDATE $? "Installing rabbitmq server"
+
+systemctl enable rabbitmq-server &>>$LOG_FILE 
+VALIDATE $? "Enabling rabbitmq-server"
+
+systemctl start rabbitmq-server &>>$LOG_FILE 
+VALIDATE $? "start rabbitmq-server"
+
+rabbitmqctl add_user roboshop $RABBITMQ_PASSWD &>>$LOG_FILE 
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE 
+
+print_time | tee -a $LOG_FILE
