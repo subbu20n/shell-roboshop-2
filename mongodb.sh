112c17459a6ce7e2b92a2342b6ae8d935cac3e51
subbu <subbunandyala48@gmail.com>
2025-10-28 08:50:29 +0530

    hello WORLD

--- mongodb.sh
+++ mongodb.sh
@@ -17,7 +17,7 @@ VALIDATE $? "Enabling mongodb"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Starting mongodb"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i  's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Editing mongodb conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
