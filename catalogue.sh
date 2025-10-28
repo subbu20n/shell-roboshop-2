c466cac4976fe40567cdba2fbed5a31a0c4f397b
subbu <subbunandyala48@gmail.com>
2025-10-28 08:55:29 +0530

    hello WORLD

--- catalogue.sh
+++ catalogue.sh
@@ -14,12 +14,12 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb client"
 
 STATUS=$(mongosh --host mongodb.subbuaws.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [ $STATUS -ne 0 ]
+if [ "$STATUS" -ne 0 ]
 then
-    mongosh --host mongodb.subbuaws.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.subbuaws.site </app/db/master-data.js &>>$LOG_FILE 
     VALIDATE $? "Loading data into mongodb"
 else
     echo -e "data is already loaded ... $Y SKIPPING $N" &>>$LOG_FILE
 fi        
 
-print_time | tee -a $LOG_FILE
+print_time | tee -a $LOG_FILE 
